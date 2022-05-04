da79f8cd5a7faaa5895740a9aaeead95c2b8396f
sxiagn <936094205@qq.com>
2022-05-04 10:18:26 +0800

    mysql持续长连接处理，无连接时不中断

--- utils/mysql.js
+++ utils/mysql.js
@@ -1,12 +1,15 @@
-const mysql      = require('mysql')
+const mysql = require('mysql')
 
-const connection = mysql.createConnection({
+const sqlConfig = {
   host     : 'localhost',
   user     : 'root',
   password : 'root',
   database : 'sxiagn_website_api'
-})
-
+}
+let connection = mysql.createConnection(sqlConfig)
 connection.connect()
-
+// 监听连接失败，失败后重新连接
+connection.on('error', err =>{
+  if (err) connection = mysql.createConnection(sqlConfig)
+})
 module.exports = connection
